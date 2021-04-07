9bac70a316ce273ec72aef639d1e9bbe7f0c73e7
aasim-akhtar <xpaasim@gmail.com>
2021-04-07 14:13:25 +0530

    Initial commit

---
+++ app/src/main/java/com/aasimakhtar/a2xmultiplier/MyWorker.java
@@ -0,0 +1,8 @@
+package com.aasimakhtar.a2xmultiplier;
+
+public class MyWorker {
+    public static int doubleTheValue(int value){
+        return (value << 1);
+//        using left shift operator once in order to multiply the value by 2
+    }
+}
